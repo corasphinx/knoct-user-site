5182c76e512b498062fcea3e7945d51684ce4a78
SPHINX <corasphinxy@gmail.com>
2021-12-07 12:52:50 -0800

    init project

---
+++ src/components/components/wallet.js
@@ -0,0 +1,136 @@
+import React from "react";
+
+const Wallet = () => (
+  <div className="row">
+    <div className="col-lg-3 mb30">
+      <span className="box-url">
+        {/*<span className="box-url-label">Most Popular</span>*/}
+        <img
+          src="./img/meta.png"
+          alt=""
+          className="mb20"
+          style={{ maxWidth: "10vw" }}
+        />
+        <h4 className="text-center">Meta</h4>
+        {/*<p>
+          Start exploring blockchain applications in seconds. Trusted by over 1
+          million users worldwide.
+        </p>*/}
+      </span>
+    </div>
+
+    <div className="col-lg-3 mb30">
+      <span className="box-url">
+        <img
+          src="./img/snapchat.jpg"
+          alt=""
+          className="mb20"
+          style={{ maxWidth: "10vw" }}
+        />
+        <h4>SnapChat</h4>
+        {/*<p>
+          Bitski connects communities, creators and brands through unique,
+          ownable digital content.
+        </p>*/}
+      </span>
+    </div>
+
+    <div className="col-lg-3 mb30">
+      <span className="box-url">
+        <img
+          src="./img/telegram.png"
+          alt=""
+          className="mb20"
+          style={{ maxWidth: "10vw" }}
+        />
+        <h4>Telegram</h4>
+        {/* <p>
+          Let users access your Ethereum app from anywhere. No more browser
+          extensions.
+        </p>*/}
+      </span>
+    </div>
+
+    <div className="col-lg-3 mb30">
+      <span className="box-url">
+        <img
+          src="./img/instagram.png"
+          alt=""
+          className="mb20"
+          style={{ maxWidth: "10vw" }}
+        />
+        <h4>Instagram</h4>
+        {/*<p>
+          Open source protocol for connecting decentralised applications to
+          mobile wallets.
+        </p>*/}
+      </span>
+    </div>
+
+    <div className="col-lg-3 mb30">
+      <span className="box-url">
+        <img
+          src="./img/nus.jpg"
+          alt=""
+          className="mb20"
+          style={{ maxWidth: "10vw" }}
+        />
+        <h4>NUS</h4>
+        {/* <p>
+          The easiest and most secure crypto wallet. ... No Coinbase account
+          required.
+        </p>*/}
+      </span>
+    </div>
+
+    <div className="col-lg-3 mb30">
+      <span className="box-url">
+        <img
+          src="./img/meta.png"
+          alt=""
+          className="mb20"
+          style={{ maxWidth: "10vw" }}
+        />
+        <h4>Meta</h4>
+        {/*<p>
+          Make it easy to create blockchain applications with secure wallets
+          solutions.
+        </p>*/}
+      </span>
+    </div>
+
+    <div className="col-lg-3 mb30">
+      <span className="box-url">
+        <img
+          src="./img/snapchat.jpg"
+          alt=""
+          className="mb20"
+          style={{ maxWidth: "10vw" }}
+        />
+        <h4>Snapchat</h4>
+        {/* <p>
+          Your wallet where you want it. Log into your favorite dapps with
+          Authereum.
+        </p>*/}
+      </span>
+    </div>
+
+    <div className="col-lg-3 mb30">
+      <span className="box-url">
+        {/*<span className="box-url-label">Most Simple</span>*/}
+        <img
+          src="./img/telegram.png"
+          alt=""
+          className="mb20"
+          style={{ maxWidth: "10vw" }}
+        />
+        <h4>Telegram</h4>
+        {/*<p>
+          Open source protocol for connecting decentralised applications to
+          mobile wallets.
+        </p>*/}
+      </span>
+    </div>
+  </div>
+);
+export default Wallet;
